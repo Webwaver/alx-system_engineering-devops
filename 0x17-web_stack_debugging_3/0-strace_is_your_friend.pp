ea9e4a39711c5c10ae2c50183becfb8ca0e5052f
Eze Favour Iheanyichukwu <39442574+biggy12@users.noreply.github.com>
2024-05-10 04:53:57 +0100

    Add files via upload

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Manifest replaces a line in a file on a server
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+$edit_file = '/var/www/html/wp-settings.php'
+
+# Editing the line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${edit_file}",
+  path    => ['/bin','/usr/bin']
 }
